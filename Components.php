f1903d38a6cbc35ed32ef002a45d2d91bb232136
misnaaslam <65425726+misnaaslam@users.noreply.github.com>
2024-04-13 16:49:31 +0530

    Update Components.php

--- Components.php
+++ Components.php
@@ -111,19 +111,7 @@
                       <li><hr class='dropdown-divider'></li
                         <hr>
                         <div class='dropdown pb-4'>
-                            <a href='#' class='d-flex align-items-center text-white text-decoration-none dropdown-toggle' id='dropdownUser1' data-bs-toggle='dropdown' aria-expanded='false'>
-                                <img src='image\profile.png' alt='hugenerd' width='30' height='30' class='rounded-circle'>
-                                <span class='d-none d-sm-inline mx-1'>cleanroad</span>
-                            </a>
-                            <ul class='dropdown-menu dropdown-menu-dark text-small shadow'>
-                                <li><a class='dropdown-item' href='#'>New project...</a></li>
-                                <li><a class='dropdown-item' href='#'>Settings</a></li>
-                                <li><a class='dropdown-item' href='#'>Profile</a></li>
-                                <li>
-                                    <hr class='dropdown-divider'>
-                                </li>
-                                <li><a class='dropdown-item' href='#'>Sign out</a></li>
-                            </ul>
+                            
                         </div>
                     </div>
                 </div>
