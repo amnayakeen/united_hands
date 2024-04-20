95a760157aa58cee897adf2ba9ffd1580bc95215
FathimaMubarack <165816074+FathimaMubarack@users.noreply.github.com>
2024-04-20 08:01:02 +0400

    Update Components.php

--- Components.php
+++ Components.php
@@ -198,6 +198,26 @@
                         </nav>
                     </div>");
             }
+		    else if($this->rec['type']=="admin")
+            {
+                echo("<script src='script.js'></script>
+                    <div class='l-navbar' id='nav-bar'>
+                        <nav class='nav'>
+                            <div> <a href='index.php' class='nav_logo'><img class='bi me-2' width='27' height='' src='Images/Logo-icon.png'><span class='nav_logo-name'>United Hands</span> </a>
+                                <div class='nav_list'>
+                                    <a href='#' class='nav_link active'  style='margin-bottom: 10px'><i class='bx bx-grid-alt nav_icon'></i> <span class='nav_name'>Dashboard</span> </a>
+                                    <a href='admin-dashboard.php' class='nav_link' style='margin-bottom: 10px'><i class='bx bx-user nav_icon'></i> <span class='nav_name'>Profile</span> </a>
+                                    <a href='view_users.php' class='nav_link' style='margin-bottom: 10px'><i class='bx bx-user-check nav_icon'></i> <span class='nav_name'>Manage Users</span> </a>
+                                    <a href='chat.php' class='nav_link' style='margin-bottom: 10px'><i class='bx bx-message-square-detail nav_icon'></i> <span class='nav_name'>Private Chats</span> </a>
+                                    <a href='groupchat.php' class='nav_link' style='margin-bottom: 10px'><i class='bx bx-chat nav_icon'></i> <span class='nav_name'>Group Chats</span> </a>
+                                    <a href='upcoming-events.php' class='nav_link' style='margin-bottom: 10px'><i class='bx bx-hourglass nav_icon'></i> <span class='nav_name'>Upcoming Events</span> </a>
+                                    <a href='#' class='nav_link' style='margin-bottom: 10px'><i class='bx bx-star nav_icon'></i> <span class='nav_name'>Rate and Review</span> </a>
+                                    <a href='reports.php' class='nav_link' style='margin-bottom: 10px'><i class='bx bx-file-blank nav_icon'></i> <span class='nav_name'>Reports</span> </a>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>");
+            }
             else
             {
                 echo("<script src='script.js'></script>
